9906afea1944d09a860df79984bc473f2b6ff12b
carlos romulo vilela silva <cromulo.romulo@gmail.com>
2024-01-28 21:44:45 -0300

    subindo app

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-var num = 10
-console.log(num)
+/* vamos criar um array com quatro tipos de cores*/
+//                 0      1           2               3
+const colors = ["green","red","rgba(133,122,200)","#f15025"]
+
+/*pegar o valor botao */
+
+const btn = document.getElementById('btn')
+
+const color = document.querySelector('.color')
+
+/*configurar um ouvinte de eventos click*/
+btn.addEventListener('click', ()=> {
+    // get random number between 0 - 3 colors [o]
+const randomNumber = 3
+
+// dentro do meu body estou estilizando o background = com o array de cores 
+document.body.style.backgroundColor = colors[randomNumber]
+// pegando a informação do array
+color.textContent = colors[randomNumber]
+})
